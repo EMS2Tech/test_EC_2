27a81a36088bea9d7f1101f743e610e8b08e0efb
Sahan Sandaruwan <138853663+Sahan-99@users.noreply.github.com>
2025-05-22 01:23:31 +0530

    logo add email verification page

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -16,6 +16,16 @@
                                 <div class="card-body">
                                     <div class="mb-0 p-0 p-lg-3">
                                         <div class="mb-0 border-0 p-md-4 p-lg-0">
+                                            <div class="mb-4 p-0">
+                                                <div class="auth-brand">
+                                                    <a class='logo logo-dark'>
+                                                        <span class="logo-lg">
+                                                            <img src="{{ asset('frontend/assets/images/ec_logo.webp') }}"
+                                                                alt="" height="50">
+                                                        </span>
+                                                    </a>
+                                                </div>
+                                            </div>
                                             
 
                                             <div class="auth-title-section mb-4 text-center">
@@ -70,25 +80,6 @@
                         <div class="auth-user-review text-center">
                             <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
                                 <div class="carousel-inner">
-
-                                    <div class="carousel-item active">
-                                        <p class="prelead mb-2">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25"
-                                                viewBox="0 0 24 24">
-                                                <path fill="#ffffff"
-                                                    d="M4.583 17.321C3.553 16.227 3 15 3 13.011c0-3.5 2.457-6.637 6.03-8.188l.893 1.378c-3.335 1.804-3.987 4.145-4.247 5.621c.537-.278 1.24-.375 1.929-.311c1.804.167 3.226 1.648 3.226 3.489a3.5 3.5 0 0 1-3.5 3.5a3.87 3.87 0 0 1-2.748-1.179m10 0C13.553 16.227 13 15 13 13.011c0-3.5 2.457-6.637 6.03-8.188l.893 1.378c-3.335 1.804-3.987 4.145-4.247 5.621c.537-.278 1.24-.375 1.929-.311c1.804.167 3.226 1.648 3.226 3.489a3.5 3.5 0 0 1-3.5 3.5a3.87 3.87 0 0 1-2.748-1.179" />
-                                            </svg>
-                                            With Untitled, your support process can be as enjoyable as your product. With
-                                            it's this easy, customers keep coming back.
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25"
-                                                viewBox="0 0 24 24">
-                                                <path fill="#ffffff"
-                                                    d="M19.417 6.679C20.447 7.773 21 9 21 10.989c0 3.5-2.456 6.637-6.03 8.188l-.893-1.378c3.335-1.804 3.987-4.145 4.248-5.621c-.537.278-1.24.375-1.93.311c-1.804-.167-3.226-1.648-3.226-3.489a3.5 3.5 0 0 1 3.5-3.5c1.073 0 2.1.49 2.748 1.179m-10 0C10.447 7.773 11 9 11 10.989c0 3.5-2.456 6.637-6.03 8.188l-.893-1.378c3.335-1.804 3.987-4.145 4.247-5.621c-.537.278-1.24.375-1.929.311C4.591 12.323 3.17 10.842 3.17 9a3.5 3.5 0 0 1 3.5-3.5c1.073 0 2.1.49 2.748 1.179" />
-                                            </svg>
-                                        </p>
-                                        <h4 class="mb-1">Camilla Johnson</h4>
-                                        <p class="mb-0">Software Developer</p>
-                                    </div>
                                 </div>
                             </div>
                         </div>
