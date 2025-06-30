fe77f898f0b97458290f3d39973bbd77f2a62c09
Sahan Sandaruwan <138853663+Sahan-99@users.noreply.github.com>
2025-06-30 22:41:54 +0530

    create course table

---
+++ database/migrations/2025_06_30_152809_create_courses_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCoursesTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->string('code')->unique();
+            $table->unsignedBigInteger('program_id');
+            $table->string('course_name');
+            $table->string('short_name');
+            $table->timestamps();
+
+            $table->foreign('program_id')->references('id')->on('study_programs')->onDelete('cascade');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+}
